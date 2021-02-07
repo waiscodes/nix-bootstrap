5d2bd6a294d2dfffb6ea74fa74b6a5444334745f
waiscodes <b.wais@outlook.com>
2021-02-07 08:45:25 -0700

    created navbar component and rendered it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar1 from "./components/Navbar1";
 
 function App() {
   return (
     <div className='App'>
+      <Navbar1 />
       <h1>NixTech App</h1>
     </div>
   );
---
+++ src/components/Navbar1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar1 = () => {
+  return (
+    <>
+      <h3>Navbar component</h3>
+    </>
+  );
+};
+
+export default Navbar1;
