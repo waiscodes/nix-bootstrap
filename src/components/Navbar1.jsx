352d8d36362ebafec594d8ce6798cdf836bb8fad
waiscodes <b.wais@outlook.com>
2021-02-07 09:07:41 -0700

    changed to a collapsible navbar

--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -1,20 +1,40 @@
 import React from "react";
-import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 
 const Navbar1 = () => {
   return (
     <>
-      <Navbar bg='light' variant='light'>
-        <Navbar.Brand href='#home'>Navbar</Navbar.Brand>
-        <Nav className='mr-auto'>
-          <Nav.Link href='#home'>Home</Nav.Link>
-          <Nav.Link href='#features'>Features</Nav.Link>
-          <Nav.Link href='#pricing'>Pricing</Nav.Link>
-        </Nav>
-        <Form inline>
-          <FormControl type='text' placeholder='Search' className='mr-sm-2' />
-          <Button variant='outline-primary'>Search</Button>
-        </Form>
+      <Navbar collapseOnSelect expand='md' bg='clear' variant='dark'>
+        <Navbar.Brand href='#home'>NixTech</Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='mr-auto'>
+            <Nav.Link href='#features'>Features</Nav.Link>
+            <Nav.Link href='#pricing'>Pricing</Nav.Link>
+            <NavDropdown title='Dropdown' id='collasible-nav-dropdown'>
+              <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.2'>
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href='#action/3.4'>
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type='text' placeholder='Search' className='mr-sm-2' />
+            <Button variant='outline-primary'>Search</Button>
+          </Form>
+        </Navbar.Collapse>
       </Navbar>
     </>
   );
